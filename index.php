3fbc34b5b0638c118d34029f0c4172d757cc123f
Lucas Magnabosco <lucasmagnabosco@live.com>
2025-06-01 14:53:10 -0300

    Adiciona permissões

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ session_start();
 // Verifica se o usuário está logado
 if (isset($_SESSION['usuario_id'])) {
     // Se estiver logado, redireciona para a página principal do sistema
-    //header("Location: .php");
+    header("Location: visualiza_produtos.php");
+    exit();
 } else {
     // Se não estiver logado, redireciona para a página de login
     header("Location: login/login.php");
